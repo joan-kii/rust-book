2dcfcdb474f1cf013c86af9f87426ab51823fac1
joan-kii <joan.sb@hotmail.com>
2022-11-19 08:00:54 +0100

    bug add "ay" with vowels

--- projects/pig_latin/src/main.rs
+++ projects/pig_latin/src/main.rs
@@ -2,9 +2,34 @@ use std::io;
 
 fn main() {
     println!("Say something!");
+
+   let vowels = vec!['a', 'e', 'i', 'o', 'u'];
     let mut words = String::new();
+
     io::stdin()
         .read_line(&mut words)
         .expect("Failed to read input");
-    println!("{words}");
+    
+    let mut word_list = Vec::new();
+    let mut word = String::new();
+
+    for letter in words.chars() {
+        if letter != ' ' {
+            word.push(letter);
+        } else if letter == ' ' {
+            word_list.push(word);
+            word = String::from("");
+        }
+    }    
+    
+    word_list.push(word);
+     
+    for mut word in word_list {
+        if vowels.iter().any(|&val| word.starts_with(val)) {
+            word += "ay";
+        }
+        println!("{word}");
+    }
+
+    
 }
