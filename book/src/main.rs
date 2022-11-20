bb462a69bb8e27db1291480c1a4fc20e24a572df
joan-kii <joan.sb@hotmail.com>
2022-11-20 12:01:58 +0100

    chapter 10

--- book/src/main.rs
+++ book/src/main.rs
@@ -1,9 +1,23 @@
-use std::collections::HashMap;
+struct Item<T, U> {
+    Number: T,
+    Char: U
+}
 
 fn main() {
-    let team_1: String = String::from("Blue");
-    let score: String = String::from("50");
+    let numbers = vec![34, 50, 25, 100, 65];
+    println!("The largest number is: {}", largest(&numbers));
 
-    let mut teams: HashMap<String, String> = HashMap::new();
-    teams.insert(team_1, score);
+    let chars = vec!['c', 'y', 'w', 'a', 'h'];
+    println!("The largest character is: {}", largest(&chars));
+}
+
+fn largest<Item>(list: &[Item<T, U>]) -> &<Item> {
+    let mut max = &list[0];
+
+    for item in list {
+        if item > max {
+            max = item;
+        }
+    }
+    max
 }
