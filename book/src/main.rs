3a6716aaf54ceeecebfef2eb58a8b26768f13bdf
joan-kii <joan.sb@hotmail.com>
2022-12-17 07:33:22 +0100

    chapter 17

--- book/src/main.rs
+++ book/src/main.rs
@@ -1,11 +1,22 @@
+use std::sync::{Arc, Mutex};
 use std::thread;
 
 fn main() {
+    let counter = Arc::new(Mutex::new(0));
+    let mut handles = vec![];
 
-    let v = vec![1, 2, 3];
-    let handle = thread::spawn(move || {
-        println!("here is a vector {:?} for the spawned thread", v);
-    });
+    for _ in 0..10 {
+        let counter = Arc::clone(&counter);
+        let handle = thread::spawn(move || {
+            let mut num = counter.lock().unwrap();
+            *num += 1;
+        });
 
-    handle.join().unwrap();
+        handles.push(handle);
+    }
+
+    for handle in handles {
+        handle.join().unwrap();
+    }
+    println!("Result: {}", *counter.lock().unwrap());
 }
